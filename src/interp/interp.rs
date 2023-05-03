ab579c842bb5ee79ec2261853a994aa95ea96bff
Zachary <zachness2003@gmail.com>
2023-05-03 18:25:27 -0500

    Splig functions

--- src/interp/interp.rs
+++ src/interp/interp.rs
@@ -17,53 +17,80 @@ impl<'a> Interp<'a> {
 
     }
 
+    fn move_memory_left(&mut self) {
+
+        // Wrap to end
+        if self.ptr == 0 {
+
+            self.ptr = self.memory.len() - 1;
+
+        } else {
+
+            self.ptr -= 1;
+
+        }
+
+    }
+
+    fn move_memory_right(&mut self) {
+
+        // Wrap to begining
+        if self.ptr == self.memory.len() - 1{
+
+            self.ptr = 0;
+
+        } else {
+
+            self.ptr += 1;
+
+        }
+
+    }
+
+    fn increment_memory(&mut self) {
+
+        self.memory[self.ptr] += 1
+
+    }
+
+    fn decrement_memory(&mut self) -> Result<(), &'static str> {
+
+        // Make sure memory does not go negative
+        if self.memory[self.ptr] != 0 {
+
+            self.memory[self.ptr] -= 1
+
+        } else {
+
+            return Err("Tried to decrement memory to a negative value");
+
+        }
+
+        Ok(())
+
+    }
+
     fn parse_command(&mut self, character: char) -> Result<(), &'static str> {
 
         match character {
 
-            '<' => {
+            '<' => self.move_memory_left(), // Go left in memory
+            '>' => self.move_memory_right(), // Go right in memory
+            '+' => self.increment_memory(), // Increment memory
 
-                // Wrap to end
-                if self.ptr == 0 {
-
-                    self.ptr = self.memory.len() - 1;
-
-                } else {
-
-                    self.ptr -= 1;
-
-                }
-
-            }, // Go left in memory
-            '>' => {
-
-                // Wrap to begining
-                if self.ptr == self.memory.len() - 1{
-
-                    self.ptr = 0;
-
-                } else {
-
-                    self.ptr += 1;
-
-                }
-
-            }, // Go right in memory
-            '+' => self.memory[self.ptr] += 1, // Increment memory
+            // Decrement memory
             '-' => {
 
-                // Make sure memory does not go negative
-                if self.memory[self.ptr] != 0 {
+                // Handle if there was an error with decrementing memory
+                let command_result = self.decrement_memory();
+                if command_result.is_err() {
 
-                    self.memory[self.ptr] -= 1
-
-                } else {
-
-                    return Err("Tried to decrement memory to a negative value");
+                    return command_result;
 
                 }
 
-            }, // Decrement memory
+            },
+            
             '.' => print!("{:?}", self.memory[self.ptr] as char), // Print memory
             _ => return Err("Invalid chacter"), // Not a known character return an error
 
